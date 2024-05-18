680af38b20cb37d3a7a365c1299ef84a90495744
Cornel Mihaescu <cornu_bv@yahoo.com>
2024-05-18 23:26:08 +0300

    added small add to cart animation

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,25 +1,46 @@
-import './product-card.styles.scss'
-import Button from '../button/button.component'
-import { useDispatch, useSelector } from 'react-redux'
-import { currencySelector, cartItemsSelector } from '../../store/cart/cart-selectors'
-import { addItemToCart } from '../../store/cart/cart-reducer'
-import { updateCartItemsReducer } from '../../store/cart/cart-reducer'
+import "./product-card.styles.scss";
+import Button from "../button/button.component";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  currencySelector,
+  cartItemsSelector,
+} from "../../store/cart/cart-selectors";
+import { addItemToCart } from "../../store/cart/cart-reducer";
+import { updateCartItemsReducer } from "../../store/cart/cart-reducer";
+import { useState } from "react";
 
 export const ProductCard = ({ product }) => {
-    const { name, price, imageUrl } = product
-    const dispatch = useDispatch();
-    const currency = useSelector(currencySelector)
-    const cartItems = useSelector(cartItemsSelector)
-    const addProductToCart = () => dispatch(updateCartItemsReducer(addItemToCart(cartItems, product)))
+  const [addedToCartAnimation, setAddedToCartAnimation] = useState(false);
+  const { name, price, imageUrl } = product;
+  const dispatch = useDispatch();
+  const currency = useSelector(currencySelector);
+  const cartItems = useSelector(cartItemsSelector);
+  const handleAddProductToCartClick = () => {
+    setAddedToCartAnimation(true);
+    setTimeout(() => setAddedToCartAnimation(false), 750);
+    dispatch(updateCartItemsReducer(addItemToCart(cartItems, product)));
+  };
 
-    return (
-        <div className='product-card-container'>
-            <img src={imageUrl} alt={`${name}`}></img>
-            <div className='footer'>
-                <span className='name'>{name}</span>
-                <span className='price-and-currency'> {price} &nbsp; {currency}</span>
-            </div>
-            <Button buttonType="inverted" onClick={addProductToCart} >Add to cart</Button>
-        </div>
-    )
-}
+  return (
+    <div className="product-card-container">
+      <img src={imageUrl} alt={`${name}`}></img>
+      <div className="footer">
+        <span className="name">{name}</span>
+        <span className="price-and-currency">
+          {" "}
+          {price} &nbsp; {currency}
+        </span>
+      </div>
+      <Button
+        buttonType={
+          addedToCartAnimation
+            ? "invertedWithConfirmItemAddedAnimation"
+            : "inverted"
+        }
+        onClick={handleAddProductToCartClick}
+      >
+        {addedToCartAnimation ? "Added" : "Add to cart"}
+      </Button>
+    </div>
+  );
+};
